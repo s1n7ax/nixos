2900a76e9755063fc1108e03740100b0b756cb41
s1n7ax <srineshnisala@gmail.com>
2023-08-12 22:39:46 +0530

    feat: adding pass password manager

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,7 @@
       qt6.qtwayland
       obs-studio
       flatpak
+      pass-wayland
     ];
   };
 
@@ -118,10 +119,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
+  programs.gnupg.agent = {
+    enable = true;
   #   enableSSHSupport = true;
-  # };
+  };
 
   programs.hyprland = {
     enable = true;
