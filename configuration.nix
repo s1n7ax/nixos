4557e08b43d666209110af2e048990e7102f46f1
s1n7ax <srineshnisala@gmail.com>
2023-08-13 13:23:13 +0530

    refactor: change home-manager pkg from sys to user

--- configuration.nix
+++ configuration.nix
@@ -112,9 +112,12 @@
       lazygit
       easyeffects
       pavucontrol
+      home-manager
 
+      # rust tools
       exa
       fd
+      sd
       ripgrep
       starship
       skim
@@ -123,10 +126,8 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
