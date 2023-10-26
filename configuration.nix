ba655ff789ec39a965b7cbfa4162500fd72e237e
s1n7ax <srineshnisala@gmail.com>
2023-10-26 15:03:25 +0530

    fix: buildx error when using devcontainer

--- configuration.nix
+++ configuration.nix
@@ -121,6 +121,27 @@
   # $ nix search wget
   # environment.systemPackages = with pkgs; [
   # ];
+  # This is used to downgrade the docker version to something older
+  # Newer version causes docker buildx to throw an error
+  # More info:
+  # https://github.com/devcontainers/feature-starter/issues/72
+  nixpkgs.overlays = [
+    (
+      let
+        pinnedPkgs = import
+          (pkgs.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
+            sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
+          })
+          { };
+      in
+      final: prev: {
+        docker = pinnedPkgs.docker;
+      }
+    )
+  ];
 
   virtualisation = {
     virtualbox.host.enable = true;
