8e1fbafbc07cb1e6a1906af382016e5e775a566c
s1n7ax <srineshnisala@gmail.com>
2024-01-17 23:03:36 +0530

    feat: enable firmware updater

--- services.nix
+++ services.nix
@@ -1,6 +1,7 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
+  services.fwupd.enable = true;
   services.flatpak.enable = true;
   services.udisks2.enable = true;
   services.vnstat.enable = true;
