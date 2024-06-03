86c2e757aed3d5c9c5e2e82d5635882a3a9c3057
s1n7ax <srineshnisala@gmail.com>
2024-06-04 00:15:31 +0530

    feat: update the flake to new nixpkg 24.05 version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,14 @@
 {
   description = "My desktop system";
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -22,21 +29,26 @@
         shell = "zsh";
         locale = "en_US.UTF-8";
       };
-
-    in {
+    in
+    {
       nixosConfigurations = {
         inherit system;
         s1n7ax = nixpkgs.lib.nixosSystem {
-          modules = [
-            (./. + "/profile" + ("/" + settings.profile) + "/configuration.nix")
-          ];
-          specialArgs = { inherit pkgs pkgs-unstable home-manager settings; };
+          modules = [ (./. + "/profile" + ("/" + settings.profile) + "/configuration.nix") ];
+          specialArgs = {
+            inherit
+              pkgs
+              pkgs-unstable
+              home-manager
+              settings
+              ;
+          };
         };
       };
     };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
