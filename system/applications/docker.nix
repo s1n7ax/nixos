830a1b2fdbc69be2c3782cc161ffcd99344ee867
s1n7ax <srineshnisala@gmail.com>
2024-07-25 20:32:23 +0530

    refactor: switch to most recent docker version

--- system/applications/docker.nix
+++ system/applications/docker.nix
@@ -1,4 +1,9 @@
-{ pkgs, enableNvidia ? false, ... }: {
+{
+  pkgs,
+  enableNvidia ? false,
+  ...
+}:
+{
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -8,16 +13,16 @@
   # Newer version causes docker buildx to throw an error
   # More info:
   # https://github.com/devcontainers/feature-starter/issues/72
-  nixpkgs.overlays = [
-    (let
-      pinnedPkgs = import (pkgs.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
-        sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
-      }) { };
-    in final: prev: { docker = pinnedPkgs.docker; })
-  ];
+  # nixpkgs.overlays = [
+  #   (let
+  #     pinnedPkgs = import (pkgs.fetchFromGitHub {
+  #       owner = "NixOS";
+  #       repo = "nixpkgs";
+  #       rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
+  #       sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
+  #     }) { };
+  #   in final: prev: { docker = pinnedPkgs.docker; })
+  # ];
 
   virtualisation.docker = {
     enable = true;
