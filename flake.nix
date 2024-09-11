ec07264c1b569d06c70ae975703b529fbc29b452
s1n7ax <srineshnisala@gmail.com>
2024-09-11 12:05:04 +0530

    refactor: switch to unstable channel

--- flake.nix
+++ flake.nix
@@ -87,10 +87,10 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
