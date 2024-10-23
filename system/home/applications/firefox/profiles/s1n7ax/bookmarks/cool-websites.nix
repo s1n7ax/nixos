e8aab5a0c74aa3092550af162e5dff028f86add2
s1n7ax <srineshnisala@gmail.com>
2024-10-23 07:46:22 +0530

    chore: bookmarks

---
+++ system/home/applications/firefox/profiles/s1n7ax/bookmarks/cool-websites.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  programs.firefox.profiles.s1n7ax.bookmarks = [
+    {
+      name = "UI";
+      bookmarks = [
+        {
+          name = "Components";
+          bookmarks = [
+            {
+              name = "Aceternity";
+              tags = [
+                "ui"
+                "components"
+              ];
+              url = "https://ui.aceternity.com/";
+            }
+          ];
+        }
+      ];
+    }
+  ];
+}
