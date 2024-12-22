ea9447e21c7de10380af7b23477c38291b04619a
s1n7ax <srineshnisala@gmail.com>
2024-12-22 15:36:05 +0530

    feat: add keyboard input fix for st

--- system/home/applications/st/st.nix
+++ system/home/applications/st/st.nix
@@ -16,6 +16,10 @@
           url = "https://st.suckless.org/patches/ligatures/0.9.2/st-ligatures-scrollback-ringbuffer-20240427-0.9.2.diff";
           hash = "sha256-E25LQ27JsKFm8yRRIpGVABJCzRnCpYo0bUdmfwo1DfU=";
         })
+        (fetchpatch {
+          url = "https://st.suckless.org/patches/fix_keyboard_input/st-fix-keyboard-input-20180605-dc3b5ba.diff";
+          hash = "sha256-h5ZrCj4IrkMQanLSMmgXaRd7qZYqvbzqUnuFt/axsMI=";
+        })
       ];
       # version controlled config file
       configFile = writeText "config.def.h" (builtins.readFile ./config.h);
