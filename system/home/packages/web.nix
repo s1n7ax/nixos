24df0d6a9fbb86c63c5b0a518edda193dbce25ba
s1n7ax <srineshnisala@gmail.com>
2025-01-31 16:30:00 +0530

    chore: remove chromium

--- system/home/packages/web.nix
+++ system/home/packages/web.nix
@@ -1,11 +1,5 @@
-{
-  lib,
-  pkgs,
-  ...
-}:
+{ lib, ... }:
 {
   options.package.web.enable = lib.mkEnableOption "web browser";
   imports = [ ../applications/firefox ];
-
-  config.home.packages = with pkgs; [ chromium ];
 }
