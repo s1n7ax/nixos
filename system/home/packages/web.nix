a711543dab822e030df6d3c985af1b31db58847d
s1n7ax <srineshnisala@gmail.com>
2025-02-27 20:43:30 +0530

    feat: add chromium to browser list

--- system/home/packages/web.nix
+++ system/home/packages/web.nix
@@ -1,5 +1,11 @@
-{ lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   options.package.web.enable = lib.mkEnableOption "web browser";
   imports = [ ../applications/firefox ];
+  config.home.packages = lib.mkIf config.package.web.enable [ pkgs.chromium ];
 }
