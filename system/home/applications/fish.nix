6de478d159a7109d8ce51ef4156e59a50048f275
s1n7ax <srineshnisala@gmail.com>
2025-04-20 22:22:52 +0530

    feat: add fish keymaps

--- system/home/applications/fish.nix
+++ system/home/applications/fish.nix
@@ -3,8 +3,17 @@
   programs.fish = {
     enable = true;
     shellInit = ''
+      stty -ixon
+
       # disable greeting
       set -g fish_greeting
+
+
+      bind ctrl-n backward-word
+      bind ctrl-e forward-word
+      bind ctrl-w backward-kill-word
+      bind ctrl-a beginning-of-line
+      bind ctrl-o end-of-line
     '';
   };
 }
