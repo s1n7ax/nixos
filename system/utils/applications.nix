8f1020ae0eff444b8b0a4b48b2c3a251245279ec
s1n7ax <srineshnisala@gmail.com>
2025-05-24 18:40:42 +0530

    fix: correct dconf option is not used to install dconf

--- system/utils/applications.nix
+++ system/utils/applications.nix
@@ -4,12 +4,12 @@ with lib;
 
 {
   programs.kdeconnect.enable = mkIf config.features.desktop.kdeconnect.enable true;
-  programs.dconf.enable = mkIf config.features.desktop.enable true;
+  programs.dconf.enable = mkIf config.features.desktop.dconf.enable true;
 
   programs.gnupg.agent = mkIf config.features.security.gpg.enable {
     enable = true;
   };
-  
+
   programs.hyprland = mkIf config.features.desktop.hyprland.enable {
     enable = true;
     xwayland.enable = true;
