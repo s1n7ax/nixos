11ed14a15042d4c3ca793b359a9313213fe285a2
s1n7ax <srineshnisala@gmail.com>
2025-05-24 10:54:47 +0530

    fix: pnpm cannot install global packages

--- system/home/packages/dev/javascript.nix
+++ system/home/packages/dev/javascript.nix
@@ -8,6 +8,10 @@
   options.package.dev.javascript.enable = lib.mkEnableOption "javascript depelopment environment";
 
   config = lib.mkIf config.package.dev.javascript.enable {
+    home.sessionVariables = {
+      PNPM_HOME = "$HOME/.local/share/pnpm";
+      PATH = "$HOME/.local/share/pnpm:$PATH";
+    };
     home.packages = with pkgs; [
       deno
       nodejs_22
