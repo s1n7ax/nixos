6c37d5c0e21354bc6764dda41883e89e0dfd3148
s1n7ax <srineshnisala@gmail.com>
2025-05-25 21:36:48 +0530

    feat: add pet snippt tool

---
+++ system/home-manager/applications/pet.nix
@@ -0,0 +1,28 @@
+{ ... }:
+{
+  programs.pet = {
+    enable = true;
+    snippets = [
+      {
+        description = "Stop all docker containers";
+        command = "docker container stop $(docker container ls -aq)";
+        tag = [ "docker" ];
+      }
+      {
+        description = "Remove all docker containers and volumes";
+        command = "docker rm -vf $(docker ps -aq)";
+        tag = [ "docker" ];
+      }
+      {
+        description = "Remove all docker images";
+        command = "docker rmi -f $(docker images -aq)";
+        tag = [ "docker" ];
+      }
+      {
+        description = "";
+        command = "docker rmi -f $(docker images -aq)";
+        tag = [ "git" ];
+      }
+    ];
+  };
+}
--- system/home-manager/packages/utility.nix
+++ system/home-manager/packages/utility.nix
@@ -14,6 +14,7 @@
     ../applications/htop.nix
     ../applications/alias.nix
     ../applications/ssh.nix
+    ../applications/pet.nix
   ];
 
   services.kdeconnect.enable = true;
