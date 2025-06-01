dd00153adb5764735a30ee3438914cf2079a25b1
s1n7ax <srineshnisala@gmail.com>
2025-06-02 01:57:36 +0530

    chore: only shallow clone

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     secrets = {
-      url = "git+ssh://git@github.com/s1n7ax/nix-secrets.git?ref=main";
+      url = "git+ssh://git@github.com/s1n7ax/nix-secrets.git?ref=main&shallow=1";
       flake = false;
     };
   };
