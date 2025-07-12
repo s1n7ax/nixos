61efc408b0aed73bad2777e380a5364313475d89
s1n7ax <srineshnisala@gmail.com>
2025-07-12 11:09:58 +0530

    chore: switch to stable neovim

--- system/home-manager/applications/neovim.nix
+++ system/home-manager/applications/neovim.nix
@@ -1,8 +1,8 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.neovim = {
     enable = true;
-    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+    # package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
