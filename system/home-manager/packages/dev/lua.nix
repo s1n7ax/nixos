ba5fbb8d7b57cc031242a7222015811cd5de13dd
s1n7ax <srineshnisala@gmail.com>
2025-11-08 12:04:32 +0530

    refactor(lua): use unstable stylua package

--- system/home-manager/packages/dev/lua.nix
+++ system/home-manager/packages/dev/lua.nix
@@ -2,12 +2,13 @@
   lib,
   config,
   pkgs,
+  pkgs-unstable,
   ...
 }:
 {
   config = lib.mkIf config.features.development.lua.enable {
     home.packages = with pkgs; [
-      stylua
+      pkgs-unstable.stylua
       lua-language-server
       luajitPackages.luacheck
     ];
