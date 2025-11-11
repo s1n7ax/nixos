4d87e2c99336905bd6ec0335878030037eaa019c
s1n7ax <srineshnisala@gmail.com>
2025-11-11 08:49:58 +0530

    refactor(hyprland): simplify animations

--- system/home-manager/applications/hyprland.nix
+++ system/home-manager/applications/hyprland.nix
@@ -135,15 +135,16 @@ in
 
         # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        # bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
 
         animation = [
-          "windows, 1, 1, myBezier"
-          "windowsOut, 1, 1, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 1, default"
-          "workspaces, 1, 1, default"
+          "workspaces, 1, 1.5, default"
+          "windows, 1, 1.5, default, slide"
+          # "windowsIn, 1, 2, default, popin 80%"
+          # "windowsOut, 1, 2, default, popin 80%"
+          # "border, 1, 10, default"
+          # "borderangle, 1, 8, default"
+          # "fade, 1, 1, default"
         ];
       };
 
