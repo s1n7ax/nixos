73a0a1f538e2d316b5a7bcef37f3e0328bc64cc2
s1n7ax <srineshnisala@gmail.com>
2026-04-24 00:34:13 +0530

    feat(kitty): add session keybindings for neovim, homelab, and nixos

--- system/home-manager/applications/kitty.nix
+++ system/home-manager/applications/kitty.nix
@@ -1,4 +1,17 @@
-{ config, ... }:
+{ pkgs, config, ... }:
+let
+  neovim_session = pkgs.writeText "neovim.kitty-session" ''
+    cd ~/.config/nvim
+    launch --title "Neovim Config" nvim
+  '';
+  homelab_session = pkgs.writeText "homelab.kitty-session" ''
+    launch --title "Homelab" kitty +kitten ssh s1n7ax@192.168.1.110
+  '';
+  nix_session = pkgs.writeText "homelab.kitty-session" ''
+    cd ~/Workspace/nixos
+    launch --title "Nixos" nvim
+  '';
+in
 {
   programs.kitty = {
     enable = true;
@@ -7,6 +20,7 @@
       name = config.settings.font.name;
       size = config.settings.font.size;
     };
+
     settings = {
       "ctrl+c" = "copy_or_interrupt";
       cursor_blink_interval = 0;
@@ -16,9 +30,16 @@
       cursor_trail_start_threshold = 0;
       confirm_os_window_close = 0;
     };
+
     themeFile = "Catppuccin-Mocha";
 
     shellIntegration.enableZshIntegration = config.settings.shell == "zsh";
     shellIntegration.enableFishIntegration = config.settings.shell == "fish";
+
+    keybindings = {
+      "alt+s>n" = "goto_session ${neovim_session}";
+      "alt+s>t" = "goto_session ${homelab_session}";
+      "alt+s>e" = "goto_session ${nix_session}";
+    };
   };
 }
