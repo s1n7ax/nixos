35fec6b3e143a5a2a0574e7cc57f3cf0d54215ce
s1n7ax <srineshnisala@gmail.com>
2026-05-30 09:11:00 +0530

    chore: add known hosts

--- system/nixos/profile/desktop/ssh.nix
+++ system/nixos/profile/desktop/ssh.nix
@@ -6,10 +6,17 @@
         hostNames = [ "github.com" ];
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       };
+
+      "codeberg.org ssh-ed25519" = {
+        hostName = [ "codeberg.org" ];
+        publicKey = "SHA256:mIlxA9k46MmM6qdJOdMnAQpzGxF4WIVVL+fj+wZbw0g";
+      };
+
       "192.168.1.110 ssh-ed25519" = {
         hostNames = [ "192.168.1.110 " ];
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHAsksMYthB3wMx9p1PQU/WNtVdfVt3dNXIE/CXUKB45";
       };
+
     };
   };
 }
